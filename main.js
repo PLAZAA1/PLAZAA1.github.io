760b43515d02c021727a35d7ebafa80b1e90fdf5
Shruti Sinha <shruti.sinha18060@gmail.com>
2021-05-28 17:53:09 +0530

    finish website

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+var currElem = null; //will hold the element that is bold now
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
+ function BoldText(elem) {
+ 
+  if (elem != currElem) { //do thing if you're clicking on a bold link
+
+   if (currElem) //if a link bold right now unbold it
+
+	currElem.style.fontWeight='normal';
+
+   currElem = elem; //assign this element as the current one
+
+   elem.style.fontWeight='bold';  //make the element clicked bold
+ 
+  }
+
+ }
